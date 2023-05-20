1e2c1cfa8886f8100cd0048feee181b098c26617
Aquib Ajani <aquibajani17@gmail.com>
2023-05-20 23:21:03 +0530

    Add features

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const PORT = 3001;
+
+const mongodbURL =
+  "mongodb+srv://aquibajanikh:aquibajanikh@cluster0.xgjdncd.mongodb.net/CourseData?retryWrites=true&w=majority";
+
+mongoose.connect(mongodbURL).then(() => console.log("Connected to db"));
+
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    title: String,
+    price: String,
+    duration: String,
+    category: String,
+  })
+);
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello!</h1>");
+app.get("/", async function (req, res) {
+  res.json(await Course.find({}));
+});
+
+app.post("/addCourse", async function (req, res) {
+  const newCourse = new Course(req.body);
+  await newCourse.save();
+  res.send({ message: "Success!" });
 });
 
 app.listen(PORT, () => {
